66e359a870554f1e25b200ae09b92ffa12757ddb
이지영 <wldudwldhd@naver.com>
2022-11-09 14:01:51 +0900

    feat:mypage페이지 분류

--- callsign/accounts/urls.py
+++ callsign/accounts/urls.py
@@ -6,6 +6,10 @@ app_name = "accounts"
 urlpatterns = [
     path('logout/', logout, name="logout"),
     path('mypage/', mypage, name="mypage"),
+        
+    path('mypost_list/', mypostlist, name="mypost_list"),
+    path('mylikepost_list/', mylikelist, name="mylikepost_list"),
+    
     path('newinfo/', newinfo, name= 'newinfo'),
     path('information/', information, name="information"),
 ]
